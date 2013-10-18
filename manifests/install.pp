d809a1768b9ffcea23fe491d411f9d8f4fdc674a
Jarl Stefansson <jarl@sdelements.com>
2013-10-18 16:44:25 -0400

    Install pyasn and pycrypto from packages

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,16 +1,19 @@
 class python_keyczar::install(
   $keyczart_binary,
 ) {
-  package { ['pyasn1', 'python-keyczar']:
+  package { ['python-keyczar']:
     ensure => latest,
     provider => pip,
     require => Class['python_keyczar::install_pip'],
+  }
+  package { ['python-crypto', 'pyasn1']
+    ensure => latest,
   }
   file { $keyczart_binary:
     ensure => file,
     mode => 0777,
     source => "puppet:///modules/python_keyczar/keyczart",
     replace => false,
-    require => Package['python-keyczar'],
+    require => Package['python-keyczar', 'python-crypto', 'pyasn1'],
   }
 }

619a37d12db2475644f9330ba826a06a63c53546
Jarl Stefansson <jarl@sdelements.com>
2013-10-18 10:04:19 -0400

    Add install

---
+++ manifests/install.pp
@@ -0,0 +1,16 @@
+class python_keyczar::install(
+  $keyczart_binary,
+) {
+  package { 'python-keyczar':
+    ensure => latest,
+    provider => pip,
+    require => Class['python_keyczar::install_pip'],
+  }
+  file { $keyczart_binary:
+    ensure => file,
+    mode => 0777,
+    source => "puppet:///modules/python_keyczar/keyczart",
+    replace => false,
+    require => Package['python-keyczar'],
+  }
+}

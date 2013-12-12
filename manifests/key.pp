06172efad21f78558eb99a81cdd4e57c74bef496
Jarl Stefansson <jarl@sdelements.com>
2013-12-12 15:23:48 -0500

    Remove backup param to symlink

--- manifests/key.pp
+++ manifests/key.pp
@@ -35,7 +35,6 @@ define python_keyczar::key (
         target => "${key_dir}",
         replace => $replace_symlinks, # Overwrite existing symlink or not
         force => false,
-        backup => true, # Back up existing file to same directory ${file_name}.puppet-bak
       }
     }
 
